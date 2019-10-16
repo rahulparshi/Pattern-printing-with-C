67eea09bf746233da59d2d8f2cbd037f577278bc
nymika <p.nymika@gmail.com>
2019-10-16 12:05:05 +0530

    first commit

--- Main.c
+++ Main.c
@@ -102,6 +102,29 @@ int main()
                         printf(" ");
                 }
                 break;
+            case 'Y':
+                for(i=0;i<n;i++)
+                {
+                    for(j=0;j<n;j++)
+                    {
+                       if(i<n/2)
+                       {
+                          if(j==i || j==n-i-1)
+                             printf("*");
+                          else 
+                             printf(" ");
+                        }
+                        else
+                        {
+                           if(j==n/2)
+                              printf("*");
+                           else
+                               printf(" ");
+                        }
+                    }
+                    printf("\n");
+                }
+
             }
             printf("  ");
         }
